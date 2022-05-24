36ecf0543498da932c441b0ab7240475a9817479
bjih1999 <bjih1999@naver.com>
2022-05-24 22:17:28 +0900

    feat: init

---
+++ src/README.md
@@ -0,0 +1,2 @@
+# Token 기반 인증 구현해보기
+* Access Token, Refresh Token을 활용한 인증 Flow 구현
---
+++ src/main/java/me/jihyun/tokenlogin/TokenLoginApplication.java
@@ -0,0 +1,13 @@
+package me.jihyun.tokenlogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TokenLoginApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TokenLoginApplication.class, args);
+    }
+
+}
